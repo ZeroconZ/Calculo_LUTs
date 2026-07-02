566d114c8eb60ab588d867fe5391f91cf8dee969
ZeroconZ <50770389+ZeroconZ@users.noreply.github.com>
2026-07-02 23:17:12 +0200

    Update Calculo_val_LUT.py

--- Calculo_val_LUT.py
+++ Calculo_val_LUT.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.20.4"
+__generated_with = "0.23.8"
 app = marimo.App(
     width="medium",
     layout_file="layouts/Calculo_val_LUT.grid.json",
@@ -12,7 +12,7 @@ def _():
     import marimo as mo
     import numpy as np
 
-    return mo, np
+    return (mo,)
 
 
 @app.cell
@@ -107,29 +107,29 @@ def _(LUTD2):
     return
 
 
-@app.cell
-def _(np):
-    def dec_bin(numero, bits_entero=3, bits_decimal=12):
-        signo = 1 if numero < 0 else 0
-        num_abs = abs(numero)
+@app.function
+def dec_bin(numero, bits_entero=3, bits_decimal=12):
+    """"
+    signo = 1 if numero < 0 else 0
+    num_abs = abs(numero)
 
-        factor = 2 ** bits_decimal  
+    factor = 2 ** bits_decimal  
 
-        valor_entero = round(num_abs * factor)
+    valor_entero = round(num_abs * factor)
 
-        max_val = (2 ** (bits_entero + bits_decimal)) - 1
-        if valor_entero > max_val:
-            print(f"Advertencia: El número {numero} excede la capacidad de la parte entera.")
+    max_val = (2 ** (bits_entero + bits_decimal)) - 1
+    if valor_entero > max_val:
+        print(f"Advertencia: El número {numero} excede la capacidad de la parte entera.")
 
-        posicion_signo = bits_entero + bits_decimal
-        valor_final = (signo << posicion_signo) | valor_entero
+    posicion_signo = bits_entero + bits_decimal
+    valor_final = (signo << posicion_signo) | valor_entero
 
-        total_bits = 1 + bits_entero + bits_decimal
-        resultado = np.binary_repr(valor_final, width=total_bits)
-
-        return resultado
-
-    return (dec_bin,)
+    total_bits = 1 + bits_entero + bits_decimal
+    resultado = np.binary_repr(valor_final, width=total_bits)
+        
+    return resultado
+    """
+    return numero
 
 
 @app.cell
@@ -166,7 +166,7 @@ def _(mo, valores_LUTD1, valores_LUTD2, valores_LUTI, valores_LUTP):
 
 
 @app.cell
-def _(K, b, dec_bin):
+def _(K, b):
     def valores_LUTP():
         dict_LUT = {
             "00" : dec_bin(0),
@@ -181,7 +181,7 @@ def _(K, b, dec_bin):
 
 
 @app.cell
-def _(K, T, Ti, dec_bin):
+def _(K, T, Ti):
     def valores_LUTI():
         dict_LUT = {
             "00" : dec_bin(0),
@@ -196,7 +196,7 @@ def _(K, T, Ti, dec_bin):
 
 
 @app.cell
-def _(N, T, Td, dec_bin):
+def _(N, T, Td):
     def valores_LUTD1():
         dict_LUT = {
             "00" : dec_bin(0),
@@ -209,7 +209,7 @@ def _(N, T, Td, dec_bin):
 
 
 @app.cell
-def _(K, N, T, Td, dec_bin):
+def _(K, N, T, Td):
     def valores_LUTD2():
         dict_LUT = {
             "00" : dec_bin(0),
